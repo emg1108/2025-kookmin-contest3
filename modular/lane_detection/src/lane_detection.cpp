0ba92e0c28d1703c191df0abcad856e6c1601b38
Your Name <your-email@example.com>
2025-08-01 02:11:17 +0900

    Add all files from rubbercone branch

---
+++ modular/lane_detection/src/lane_detection.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main(){
+    std::cout << "I'm lane_node" << std::endl;
+    // Additional initialization code can go here
+    return 0;
+}
+
---
+++ modular/traffic_light/src/traffic_light.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main(){
+    std::cout << "I'm traffic_node" << std::endl;
+    // Additional initialization code can go here
+    return 0;
+}
